2a446f5d8791bbc02f50872d76366d8f44611d69
Ahmed Essam Mohamed <aessam72@gmail.com>
2018-12-10 22:29:04 +0200

    Update models.py

--- Request/models.py
+++ Request/models.py
@@ -5,11 +5,11 @@ from Location.models import location
 class request(models.Model):
     #Owner_id	hotel_location_id	hotel_name	count	stars	image
     #owner_id foreign key
-    hotel_location_id = models.ForeignKey(location,on_delete=models.CASCADE)
+    hotel_location_id = models.ForeignKey(location, on_delete=models.CASCADE)
     hotel_name = models.CharField(max_length=100)
     singleRoomsCount = models.IntegerField(default=0)
     doubleRoomsCount = models.IntegerField(default=0)
     singelRoomsPrice = models.IntegerField(default=0)
     doubleRoomsPrice = models.IntegerField(default=0)
-    stars = models.IntegerField
+    stars = models.IntegerField()
     image = models.ImageField(upload_to=None)
