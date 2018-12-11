ad05b8176043712694f31fc597a3bf3db3a2c190
Ahmed Essam Mohamed <aessam72@gmail.com>
2018-12-11 21:09:21 +0200

    Update models.py

--- Customer/models.py
+++ Customer/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class customer(models.Model):
-   username = models.CharField(max_length=100, blank=False) #false because required to register
+   user_name = models.CharField(max_length=100, blank=False) #false because required to register
    password = models.CharField(max_length=100, blank=False)
    email = models.EmailField(max_length=70, blank=False)
-   mobile = models.BigAutoField(max_length=30, blank=False)
+   mobile = models.BigIntegerField(blank=False)
    black_listed = models.BooleanField(default=False)
