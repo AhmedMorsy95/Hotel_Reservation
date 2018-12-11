5ee44589eefe2855f7de6c422e7f81bc4e0b0bad
Ahmed Essam Mohamed <aessam72@gmail.com>
2018-12-11 11:05:10 +0200

    Update models.py

--- Reservation/models.py
+++ Reservation/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Reservations(models.Model):
-    # customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    # room_id =  models.ForeignKey(Rooms, on_delete=models.CASCADE)
+    customer_id = models.ForeignKey(customer, on_delete=models.CASCADE)
+    room_id =  models.ForeignKey(room, on_delete=models.CASCADE)
     confirmed = models.BooleanField(default=False)
     from_date = models.DateTimeField('Date')
     to_date = models.DateTimeField('Date')
