988fbafa0aa500d1d4ae680116a12971416a6d31
Merna177 <37479454+Merna177@users.noreply.github.com>
2018-12-11 03:49:35 +0200

    update email

--- Owner/models.py
+++ Owner/models.py
@@ -6,6 +6,6 @@ from django.db import models
 class Owner(models.Model):
     Username = models.CharField(max_length=100)
     Password = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
+    email = models.EmailField(max_length=70)
     mobileNumber = models.CharField(max_length=15)
   
