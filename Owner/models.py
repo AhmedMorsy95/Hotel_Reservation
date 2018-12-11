04e431b979a9329f4612445c1f9790e8532fad41
Ahmed Essam Mohamed <aessam72@gmail.com>
2018-12-11 20:18:58 +0200

    Update models.py

--- Owner/models.py
+++ Owner/models.py
@@ -1,8 +1,5 @@
 from django.db import models
 
-# Create your models here.
-from django.db import models
-
 class Owner(models.Model):
     Username = models.CharField(max_length=100)
     Password = models.CharField(max_length=100)
